ec6f4b1a071840a80795ec33b3bc7371432d47b8
Ruben <ruben270493@gmail.com>
2019-11-01 14:54:38 +0100

    Anotacion ModelAttribute

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,12 +1,12 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -39,14 +39,18 @@ public class IndexController {
 	
 	@GetMapping("/listar")
 	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de usuarios");
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios() {
 		List<Usuario> usuarios = Arrays.asList(
 			new Usuario("Andrés","Guzman","andres@correo.com"),
 			new Usuario("Rubén","Fernández","ruben@correo.com"),
 			new Usuario("Jane","Doe","jane@correo.com"),
 			new Usuario("Anonimo","Doe","anonimo@correo.com")
 		);
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
-		return "listar";
+		return usuarios;
 	}
 }
