2f845a44b88a4f95aceb100950d3da70e15dbb43
Ruben <ruben270493@gmail.com>
2019-11-02 12:00:18 +0100

    Directiva @Primary

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -12,11 +12,12 @@ public class IndexController {
 	
 	@Autowired
 	private IServicio servicio;
-	
+
 	@GetMapping({"/","","/index"})
 	public String index(Model model) {
 		model.addAttribute("objeto", servicio.operacion());
 		return "index";
 	}
+	
 	
 }
