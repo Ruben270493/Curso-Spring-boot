09af83f40bfb7f3f01dbb3452c1523966647a656
Ruben <ruben270493@gmail.com>
2019-11-02 12:49:56 +0100

    Controlador FacturaController y su vista

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/ItemFactura.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/ItemFactura.java
@@ -13,14 +13,20 @@ public class ItemFactura {
 	public Producto getProducto() {
 		return producto;
 	}
+	
 	public void setProducto(Producto producto) {
 		this.producto = producto;
 	}
+	
 	public Integer getCantidad() {
 		return cantidad;
 	}
+	
 	public void setCantidad(Integer cantidad) {
 		this.cantidad = cantidad;
 	}
-		
+	
+	public Integer calcularImporte() {
+		return cantidad * producto.getPrecio();
+	}
 }
