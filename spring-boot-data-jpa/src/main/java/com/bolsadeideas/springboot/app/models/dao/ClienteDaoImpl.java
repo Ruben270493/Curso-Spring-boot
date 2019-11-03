52801db78e3904c775c27dab7573abd85ac6ddb3
Ruben <ruben270493@gmail.com>
2019-11-03 09:54:30 +0100

    Clase DAO con entity manager

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao{
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	@Transactional(readOnly=true)
+	@SuppressWarnings("unchecked")
+	public List<Cliente> findAll() {
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+}
