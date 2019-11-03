a89508b5e75b4a4c151d2cee4649d2a3322a72ee
Ruben <ruben270493@gmail.com>
2019-11-03 10:37:55 +0100

    Creación de metodos para insertar clientes

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 @Controller
 public class ClienteController {
@@ -20,4 +22,17 @@ public class ClienteController {
 		return "listar";
 	}
 	
+	@GetMapping(value = "/form")
+	public String crear(Model model) {
+		Cliente cliente = new Cliente();
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("titulo", "Formulario de Cliente");
+		return "form";
+	}
+	
+	@PostMapping(value = "/form")
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
+	}
 }
