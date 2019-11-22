5e4179acf5f7ec7faabb740b1ee7c0d05e20db42
Ruben <ruben270493@gmail.com>
2019-11-22 18:39:41 +0100

    Validación del formulario de login

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
@@ -5,15 +5,19 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/login")
-	public String Login(Model model, Principal principal) {
+	public String Login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal) {
 		
 		if (principal != null)
 			return "redirect:/";
+		
+		if (error != null)
+			model.addAttribute("error", "El usuario o la contraseña es incorrecta.");
 		
 		return "login";
 		
