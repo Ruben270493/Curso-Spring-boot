60b86ff381a439bdf96cb4cca7da9b69b1f6bf01
Ruben <ruben270493@gmail.com>
2019-11-23 10:48:32 +0100

    Manejo de errores de acceso

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -41,7 +41,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/factura/**").hasAnyRole("ADMIN")
 		.anyRequest().authenticated()
 		.and().formLogin().loginPage("/login").permitAll()
-		.and().logout().permitAll();
+		.and().logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403");
 		
 	}
 	
