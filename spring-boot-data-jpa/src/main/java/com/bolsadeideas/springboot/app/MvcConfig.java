e431c6ae2ff1d5ea45f1b37d06fff1e1b7187d3d
Ruben <ruben270493@gmail.com>
2019-12-05 22:40:30 +0100

    Configuración y creación de la clase XML Wrapper

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -36,21 +37,37 @@ public class MvcConfig implements WebMvcConfigurer {
 	
 	@Bean
 	public LocaleResolver localeResolver() {
+		
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
 		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+		
 		return localeResolver;
+		
 	}
 	
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
+		
 		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
 		localeInterceptor.setParamName("lang");
+		
 		return localeInterceptor;
+		
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Bean
+	public Jaxb2Marshaller jaxb2Marshaller() {
+		
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(new Class[] {com.bolsadeideas.springboot.app.view.xml.ClienteList.class});
+		
+		return marshaller;
+		
 	}
 	
 }
