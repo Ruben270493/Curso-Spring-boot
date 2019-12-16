9e8bae0019ea846b2a4cbf3ee4f2429a7625e9bd
Ruben <ruben270493@gmail.com>
2019-12-16 19:09:51 +0100

    Creación de la clase de servicio ClienteService

--- angular/clientes-app/src/app/app.module.ts
+++ angular/clientes-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import { ClientesComponent } from './clientes/clientes.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
