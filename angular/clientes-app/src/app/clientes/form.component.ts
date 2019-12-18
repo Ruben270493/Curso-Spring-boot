bae1df0a2e8154e0fbea58200aa6d1111a3b66b3
Ruben <ruben270493@gmail.com>
2019-12-18 21:33:06 +0100

    Carga de datos en el formulario para actualizar

--- angular/clientes-app/src/app/clientes/form.component.ts
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import swal from 'sweetalert2';
 
 @Component({
@@ -14,9 +14,22 @@ export class FormComponent implements OnInit {
   private cliente:Cliente = new Cliente();
   private title:string = "Crear cliente";
 
-  constructor(private clienteService:ClienteService, private router:Router) { }
+  constructor(private clienteService:ClienteService, private router:Router, private activatedRoute:ActivatedRoute) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.cargarCliente();
+  }
+
+  cargarCliente():void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if (id) {
+        this.clienteService.getCliente(id).subscribe(
+          (cliente) => this.cliente = cliente
+        )
+      }
+    });
+  }
 
   public create():void {
     this.clienteService.create(this.cliente).subscribe(
