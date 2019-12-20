8fd12b7fed9ec962b590bc73e8b1fe24d61a1e94
Ruben <ruben270493@gmail.com>
2019-12-20 22:55:44 +0100

    Implementando @Valid

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteController.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteController.java
@@ -1,13 +1,19 @@
 package com.bolsadeideas.springboot.backend.apirest.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
@@ -59,10 +64,23 @@ public class ClienteController {
 	}
 	
 	@PostMapping("/clientes/")
-	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
 		
 		Cliente nuevoCliente = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if (result.hasErrors()) {
+			
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo " + err.getField() + " '"+ err.getDefaultMessage() + "'")
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+			
+		}
 		
 		try {
 			nuevoCliente = clienteService.save(cliente);
@@ -79,11 +97,24 @@ public class ClienteController {
 	}
 	
 	@PutMapping("/clientes/{id}")
-	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, @PathVariable Long id, BindingResult result) {
 		
 		Cliente clienteActual = clienteService.findById(id);
 		Map<String, Object> response = new HashMap<>();
 		Cliente clienteActualizado = null;
+		
+		if (result.hasErrors()) {
+			
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo " + err.getField() + " '"+ err.getDefaultMessage() + "'")
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+			
+		}
 		
 		if (null == clienteActual) {
 			response.put("mensaje", "El cliente con ID '" + id + "' no existe en la base de datos.");
