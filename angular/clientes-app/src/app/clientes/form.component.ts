6aafe138c08dfadb159e02fe5e0c2ef2d320b802
Ruben <ruben270493@gmail.com>
2019-12-20 23:27:59 +0100

    Manejo de errores de validación en angular

--- angular/clientes-app/src/app/clientes/form.component.ts
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -13,6 +13,7 @@ export class FormComponent implements OnInit {
 
   private cliente:Cliente = new Cliente();
   private title:string = "Crear cliente";
+  private errores:string[];
 
   constructor(private clienteService:ClienteService, private router:Router, private activatedRoute:ActivatedRoute) { }
 
@@ -36,15 +37,27 @@ export class FormComponent implements OnInit {
       cliente => {
         this.router.navigate(['/clientes'])
         swal.fire('Nuevo cliente', `¡Cliente ${cliente.nombre} creado correctamente!`, 'success')
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error("Código de error: " + err.status);
+        console.error(err.error.errors);
       }
     );
   }
 
   public update():void {
-    this.clienteService.update(this.cliente).subscribe(json => {
-      this.router.navigate(['/clientes'])
-      swal.fire('Cliente actualizado', `El cliente ${json.cliente.nombre} se ha actualizado correctamente`, 'success')
-    });
+    this.clienteService.update(this.cliente).subscribe(
+      json => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Cliente actualizado', `El cliente ${json.cliente.nombre} se ha actualizado correctamente`, 'success')
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error("Código de error: " + err.status);
+        console.error(err.error.errors);
+      }
+    );
   }
 
 }
