cf4b561fd0dcfb0676564178e4cd7f5a2aaac9f9
Ruben <ruben270493@gmail.com>
2019-12-22 12:42:49 +0100

    Método subscribe()

--- spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
@@ -16,10 +20,15 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Flux<String> nombres = Flux.just("Rubén", "Andrés", "Trang", "Juan")
-				.doOnNext(System.out::println);
+		Flux<String> nombres = Flux.just("Rubén", "Andrés", "Trang", "Juan", "")
+				.doOnNext(e -> {
+					if (e.isEmpty())
+						throw new RuntimeException("Los nombres no pueden estar vacíos.");
+					else
+						System.out.println(e);
+				});
 		
-		nombres.subscribe();
+		nombres.subscribe(log::info, error -> log.error(error.getMessage()));
 		
 	}
 
