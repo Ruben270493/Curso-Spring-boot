8b2ea20b1fcfd36a3a409da7f34ccc34fdefaba9
Ruben <ruben270493@gmail.com>
2019-12-22 12:56:07 +0100

    Operador map

--- spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bolsadeideas.springboot.reactor.app.models.Usuario;
+
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
@@ -20,15 +22,20 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Flux<String> nombres = Flux.just("Rubén", "Andrés", "Trang", "Juan")
-				.doOnNext(e -> {
-					if (e.isEmpty())
-						throw new RuntimeException("Los nombres no pueden estar vacíos.");
-					else
-						System.out.println(e);
+		Flux<Usuario> nombres = Flux.just("Rubén", "Andrés", "Trang", "Juan")
+			.map(nombre -> new Usuario(nombre.toUpperCase(), null))
+			.doOnNext(usuario -> {
+				if (null == usuario)
+					throw new RuntimeException("Los nombres no pueden estar vacíos.");
+				else
+					System.out.println(usuario.getNombre());
+			}).map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
 				});
 		
-		nombres.subscribe(log::info, 
+		nombres.subscribe(e -> log.info(e.toString()), 
 				error -> log.error(error.getMessage()),
 				new Runnable() {
 					@Override
