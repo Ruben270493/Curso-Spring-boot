8552a19011beb4a45996eb1cfc548a994f32be8b
Ruben <ruben270493@gmail.com>
2019-12-22 19:17:33 +0100

    Clases de Modelo Document y Repository

---
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/models/dao/ProductoDao.java
@@ -0,0 +1,7 @@
+package com.bolsadeideas.springboot.webflux.models.dao;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.bolsadeideas.springboot.webflux.models.documents.Producto;
+
+public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {}
